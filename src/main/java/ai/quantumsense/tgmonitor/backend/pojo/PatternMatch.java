9012cbbe60c7328e0b2e9cbd99a6ff8631268650
Daniel Weibel <danielmweibel@gmail.com>
2018-07-10 22:05:17 +0800

    Define PatternMatch class

--- src/main/java/ai/quantumsense/tgmonitor/backend/pojo/PatternMatch.java
+++ src/main/java/ai/quantumsense/tgmonitor/backend/pojo/PatternMatch.java
@@ -1,4 +1,21 @@
 package ai.quantumsense.tgmonitor.backend.pojo;
 
+import java.util.Set;
+
 public class PatternMatch {
+    private TelegramMessage msg;
+    private Set<String> patterns;
+
+    public PatternMatch(TelegramMessage msg, Set<String> patterns) {
+        this.msg = msg;
+        this.patterns = patterns;
+    }
+
+    public TelegramMessage getMessage() {
+        return msg;
+    }
+
+    public Set<String> getPatterns() {
+        return patterns;
+    }
 }
